0bc97204124ff5d69209502da42865806761eaa3
Андрей Соловьев <gmail.com>
2025-05-24 15:48:58 +0700

    add timeout | 24.05.2025 15:48

--- app.js
+++ app.js
@@ -4,6 +4,7 @@ const app = express()
 app.use(express.json())
 app.listen(3000, () => console.log('work on Port: 3000')) */
 
-import { getTariffs } from './modules/getEntities.js'
+import { getClientsAndEquipments, getTariffs } from './modules/getEntities.js'
 
 console.log(await getTariffs())
+console.log(await getClientsAndEquipments())
--- modules/getEntities.js
+++ modules/getEntities.js
@@ -5,7 +5,7 @@ export async function getTariffs() {
 	let startId = 0
 	let result
 
-	while (result != [] && result != 'Ошибка') {
+	while (result != '' && result != 'Ошибка') {
 		let request = await fetch(
 			`https://${_account}.okdesk.ru/api/v1/nomenclature/price_lists/${_priceListId}/services?api_token=${_token}&page[size]=1&page[from_id]=${startId}&page[direction]=forward`
 		)
@@ -24,9 +24,36 @@ export async function getTariffs() {
 			result = 'Ошибка'
 			return `${request.statusText} ${request.text}`
 		}
+		await new Promise(resolve => setTimeout(resolve, 200))
 	}
-
 	return allTariffsArr
 }
 
-export async function getClientsAndEquipments() {}
+export async function getClientsAndEquipments() {
+	let billingEquipmentsArr = []
+	let startId = 0
+	let result
+
+	while (result != [] && result != 'Ошибка') {
+		let request = await fetch(
+			`https://${_account}.okdesk.ru/api/v1/nomenclature/price_lists/${_priceListId}/services?api_token=${_token}&page[size]=1&page[from_id]=${startId}&page[direction]=forward`
+		)
+		if (request.ok) {
+			let result = await request.json()
+			if (result == '') {
+				break
+			}
+			let id = result[0].id
+			let name = result[0].name
+			let price = result[0].price
+			let vendorCode = result[0].vendor_code
+			startId = id + 1
+			billingEquipmentsArr.push({ id, name, price, vendorCode })
+		} else {
+			result = 'Ошибка'
+			return `${request.statusText} ${request.text}`
+		}
+		await new Promise(resolve => setTimeout(resolve, 200))
+	}
+	return billingEquipmentsArr
+}
